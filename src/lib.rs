238ee53de642050bcc56664e01344ea844c72e3e
deltronix <douwe@deltronix.nl>
2025-03-11 18:57:47 +0100

    enabled host compilation for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 #![no_std]
 
 pub mod dsp {
-    use micromath::F32Ext;
-
+    use dasp::ring_buffer::Fixed;
     pub struct TransientDetector<const N_CHANNELS: usize, const BLOCK_SIZE: usize> {
         fast_attack: f32,
         fast_release: f32,
         slow_attack: f32,
         slow_release: f32,
-        last_fast_frame: [f32; N_CHANNELS],
-        last_slow_frame: [f32; N_CHANNELS],
+        pub ma_input: [f32; N_CHANNELS],
+        pub last_fast_frame: [f32; N_CHANNELS],
+        pub last_slow_frame: [f32; N_CHANNELS],
         pub max_diff: [f32; N_CHANNELS],
-        diff: [f32; N_CHANNELS],
-        threshold: f32,
+        pub threshold_up: f32,
+        pub threshold_down: f32,
         armed: [bool; N_CHANNELS],
         trig: [Option<bool>; N_CHANNELS],
     }
@@ -23,18 +23,20 @@ pub mod dsp {
             fast_release: f32,
             slow_attack: f32,
             slow_release: f32,
-            threshold: f32,
+            threshold_up: f32,
+            threshold_down: f32,
         ) -> Self {
             Self {
                 fast_attack: Self::calc_gain(fast_attack),
                 fast_release: Self::calc_gain(fast_release),
                 slow_attack: Self::calc_gain(slow_attack),
                 slow_release: Self::calc_gain(slow_release),
+                ma_input: [0.0; N_CHANNELS],
                 last_fast_frame: [0.0; N_CHANNELS],
                 last_slow_frame: [0.0; N_CHANNELS],
                 max_diff: [0.0; N_CHANNELS],
-                diff: [0.0; N_CHANNELS],
-                threshold,
+                threshold_up,
+                threshold_down,
                 armed: [true; N_CHANNELS],
                 trig: [None; N_CHANNELS],
             }
@@ -47,10 +49,7 @@ pub mod dsp {
             }
         }
 
-        pub fn process_block(
-            &mut self,
-            block: &[[f32; N_CHANNELS]; BLOCK_SIZE],
-        ) -> [Option<bool>; N_CHANNELS] {
+        pub fn process_block(&mut self, block: &[[f32; N_CHANNELS]]) -> [Option<bool>; N_CHANNELS] {
             self.max_diff = [0.0; N_CHANNELS];
             self.trig = [None; N_CHANNELS];
             for frame in block {
@@ -59,29 +58,30 @@ pub mod dsp {
                     let fast_diff = self.last_fast_frame[ch] - rectified;
                     let slow_diff = self.last_slow_frame[ch] - rectified;
                     if self.last_fast_frame[ch] < rectified {
-                        self.last_fast_frame[ch] = rectified + (fast_diff * self.fast_attack);
+                        self.last_fast_frame[ch] = rectified + (fast_diff * self.fast_attack)
                     } else {
-                        self.last_fast_frame[ch] = rectified + (fast_diff * self.fast_release);
+                        self.last_fast_frame[ch] = rectified + (fast_diff * self.fast_release)
                     }
                     if self.last_slow_frame[ch] < rectified {
-                        self.last_slow_frame[ch] = rectified + (slow_diff * self.slow_attack);
+                        self.last_slow_frame[ch] = rectified + (slow_diff * self.slow_attack)
                     } else {
-                        self.last_slow_frame[ch] = rectified + (slow_diff * self.slow_release);
+                        self.last_slow_frame[ch] = rectified + (slow_diff * self.slow_release)
                     }
 
                     let env_diff = self.last_fast_frame[ch] - self.last_slow_frame[ch];
-                    if env_diff > self.max_diff[ch] {
-                        self.max_diff[ch] = env_diff;
-                    }
+                    //self.max_diff[ch] = (self.max_diff[ch] + env_diff) * 0.5;
+                    self.max_diff[ch] = env_diff;
                 });
             }
             (0..N_CHANNELS).for_each(|ch| {
-                if self.armed[ch] && self.max_diff[ch] > self.threshold {
+                if self.armed[ch] && self.max_diff[ch] > self.threshold_up {
                     self.trig[ch] = Some(true);
                     self.armed[ch] = false;
-                } else if self.max_diff[ch] < self.threshold {
+                } else if !self.armed[ch] && self.max_diff[ch] < self.threshold_down {
                     self.trig[ch] = Some(false);
                     self.armed[ch] = true;
+                } else {
+                    self.trig[ch] = None;
                 }
             });
 
@@ -107,6 +107,7 @@ pub mod dsp {
     //     }
 }
 
+#[cfg(target_arch = "arm")]
 mod setup {
     use embassy_stm32::Config;
     use embassy_stm32::Peripherals;
@@ -117,22 +118,22 @@ mod setup {
 
         //config.rcc.hsi = Some(HSIPrescaler::DIV2);
         config.rcc.pll1 = Some(Pll {
-            source: PllSource::HSI,
-            prediv: PllPreDiv::DIV4,
-            mul: PllMul::MUL50,
-            divp: Some(PllDiv::DIV2),
+            source: PllSource::HSI,   // 64 MHz
+            prediv: PllPreDiv::DIV4,  // 16 MHz
+            mul: PllMul::MUL50,       // 800 MHz
+            divp: Some(PllDiv::DIV2), // 400 MHz
             divq: None,
             divr: None,
         });
         config.rcc.pll2 = Some(Pll {
-            source: PllSource::HSI,
-            prediv: PllPreDiv::DIV4,
-            mul: PllMul::MUL50,
-            divp: Some(PllDiv::DIV8), // 100mhz
+            source: PllSource::HSI,   // 64 MHz
+            prediv: PllPreDiv::DIV4,  // 16 MHz
+            mul: PllMul::MUL50,       // 800 MHz
+            divp: Some(PllDiv::DIV8), // 100MHz
             divq: None,
             divr: None,
         });
-        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.sys = Sysclk::PLL1_P; // 400 MHz
         config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
         config.rcc.apb1_pre = APBPrescaler::DIV4; // 100 Mhz
         config.rcc.apb2_pre = APBPrescaler::DIV4; // 100 Mhz
