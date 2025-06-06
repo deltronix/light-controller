e57fa9da4e2125888ae55d84963d112c74623692
deltronix <douwe@deltronix.nl>
2025-06-06 22:02:53 +0200

    analog conditioning and preamp board sent for prototype manufacture

---
+++ hardware/production.py
@@ -0,0 +1,4 @@
+kikit separate --source 'rectangle; tlx: 414mm; tly: 23mm; brx: 453mm; bry: 67mm' \
+        dmx_controller.kicad_pcb conditioning_board.kicad_pcb
+kikit separate --source 'rectangle; tlx: 456mm; tly: 23mm; brx: 504mm; bry: 67mm' \
+        dmx_controller.kicad_pcb preamp_board.kicad_pcb
